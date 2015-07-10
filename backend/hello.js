0c5f5ff5a29bff00b2f213ad4c0ea4ff5acd6829
Artem <pandos.jimmyrose@gmail.com>
2015-07-10 22:37:19 +0300

    New commit

--- backend/hello.js
+++ backend/hello.js
@@ -1,18 +1,12 @@
 var fs = require('fs');
 var http = require('http');
-var server = http.createServer();
 
-server.on('request', function(request, response) {
-    var newFile = fs.createWriteStream("../app/index.html");
-    request.pipe(newFile);
-    request.on("end",function(){
-        response.end("uploaded!");
-    });
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
 
-});
-
-
-server.listen(11111);
+    var file = fs.createReadStream('../app/index.html');
+    file.pipe(response);
+}).listen(11111);
 
 
 console.log("Listening on port 11111...");
